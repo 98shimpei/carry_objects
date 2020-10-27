aa2d06a10451beb5e518458dceeee055bf51ec42
jaxonredvision <jaxonredvision@jsk.imi.i.u-tokyo.ac.jp>
2020-10-27 14:47:09 +0900

    pub sub test

--- python/subtest.py
+++ python/subtest.py
@@ -2,7 +2,6 @@
 import rospy
 import geometry_msgs.msg
 from ar_track_alvar_msgs.msg import AlvarMarkers
-from hrpsys_ros_bridge.msg import BoxPose
 rospy.init_node('test_sub')
 def cb(msg):
     print("sub")
